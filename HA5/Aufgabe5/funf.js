37d0b4148217dcb7a628c8ea478a7fba5850476d
Tobias <dunker@campus.tu-berlin.de>
2017-11-29 21:54:20 +0000

    Aufgabe 5 verändert

--- HA5/Aufgabe5/funf.js
+++ HA5/Aufgabe5/funf.js
@@ -13,9 +13,14 @@ Vector.prototype.minus = function(vec){
     return new Vector(this.x - vec.x, this.y - vec.y)
 }
 
-Vector.prototype.length=function(){
-  return Math.sqrt(this.x*this.x+this.y*this.this.y)
-}
+Object.defineProperty(Vector.prototype,"lenght",{
+    get:function(){
+         return Math.sqrt(this.x*this.x+this.y*this.this.y)
+    }
+})
+//Vector.prototype.length=function(){
+//  return Math.sqrt(this.x*this.x+this.y*this.this.y)
+//}
 
 
 
@@ -23,43 +28,61 @@ console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
 console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 // → Vector{x: -1, y: -1}
-//console.log(new Vector(3, 4).length);
+console.log(new Vector(3, 4).length);
 // → 5
 
 
-//-----------------------------------------------------------------------------
-function StretchCell(inner, width, height){
-    this.inner = inner;
-    this.width = width;
-    this.height = height;
+//_--------------------------------------------------------
+function logFive(para){
+    for(var i = 0;i<5;i++){
+        console.log(para.wert())
+    }
 }
 
-StretchCell.prototype.minWidth= function(){
-    return
-}
-StretchCell.prototype.minHeight= function(){
-    return
-}
-StretchCell.prototype.draw= function(widht, height){
-    return
+function ArraySeq(arr){
+    this.arr= arr;
+    this.p=0
 }
 
+ArraySeq.prototype.wert = function(){
+    return this.arr[this.p];
+}
 
-var sc = new StretchCell(new TextCell("abc"), 1, 2);
-console.log(sc.minWidth());
-// → 3
-console.log(sc.minHeight());
-// → 2
-console.log(sc.draw(3, 2));
-// → ["abc", "   "]
+ArraySeq.prototype.naechster = function(){
+    if(this.p<=this.arr.length-1){
+    this.p++
+    return true
+        
+    }else{
+        return false
+    }
+}
 
-//--------------------------------------------------------
+
+function RangeSeq(start, end){
+    this.p = start;
+    this.end = end;
+}
+
+RangeSeq.prototype.naechster = function(){
+        if(this.p<=this.end){
+    this.p++
+    return true
+        
+    }else{
+        return false
+    }
+}
+
+RangeSeq.prototype.wert = function(){
+    return this.p
+}
 
 
-//logFive(new ArraySeq([1, 2]));
+logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
-//logFive(new RangeSeq(100, 1000));
+logFive(new RangeSeq(100, 1000));
 // → 100
 // → 101
 // → 102
